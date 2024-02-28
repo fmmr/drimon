58dc88b64dc0093fe9c488bab288bf642712fea2
Fredrik Rødland <fmr@finn.no>
2024-02-28 14:02:20 +0100

    added motion detection

--- esp_32_pir_motion/esp_32_pir_motion.ino
+++ esp_32_pir_motion/esp_32_pir_motion.ino
@@ -1,24 +1,49 @@
+#include "pitches.h"
+#define timeSeconds 2
+
 // the number of the LED pin
 const int ledPinPwm = 18;  // 16 corresponds to GPIO16
 int ledPins[] = { 13, 12, 14 };
 const byte chns[] = { 1, 2, 3 };
 int red, green, blue;
 
+const int PRINT_EVERY = 50;
+
 // setting PWM properties
 const int freq = 5000;
 const int ledChannel = 0;
 const int resolution = 8;
+int countPrint = 0;
 
 // set pin numbers
 const int buttonPin = 4;  // the number of the pushbutton pin
 const int ledPin = 5;     // the number of the LED pin
 const int potPin = 34;
+const int buzzer = 26;
+const int motionSensor = 27;
+unsigned long now = millis();
+unsigned long lastTrigger = 0;
+boolean startTimer = false;
+boolean motion = false;
+
+void beep(int note, int duration) {
+  tone(buzzer, note, duration);
+  delay(duration);
+}
+void IRAM_ATTR detectsMovement() {
+  digitalWrite(ledPin, HIGH);
+  startTimer = true;
+  lastTrigger = millis();
+}
 
 // variable for storing the pushbutton status
 void setup() {
   Serial.begin(115200);
   pinMode(buttonPin, INPUT);  // initialize the pushbutton pin as an input
   pinMode(ledPin, OUTPUT);    // initialize the LED pin as an output
+  pinMode(motionSensor, INPUT_PULLUP);
+  pinMode(buzzer, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(motionSensor), detectsMovement, RISING);
 
   ledcSetup(ledChannel, freq, resolution);
   ledcAttachPin(ledPinPwm, ledChannel);
@@ -30,10 +55,30 @@ void setup() {
 }
 
 void loop() {
+  now = millis();
+
+  if ((digitalRead(ledPin) == HIGH) && (motion == false)) {
+    Serial.println("MOTION DETECTED!!!");
+    beep(NOTE_A4, 50);
+    motion = true;
+  }
+  // Turn off the LED after the number of seconds defined in the timeSeconds variable
+  if (startTimer && (now - lastTrigger > (timeSeconds * 1000))) {
+    Serial.println("Motion stopped...");
+    digitalWrite(ledPin, LOW);
+    startTimer = false;
+    motion = false;
+  }
+
   checkButton();
   potMeter();
   // fadeInFadeOut();
   delay(20);
+  if (countPrint % PRINT_EVERY == 0) {
+    countPrint = 1;
+  } else {
+    countPrint++;
+  }
 }
 
 void fadeInFadeOut() {
@@ -54,28 +99,35 @@ int potMeter() {
   int potValue = analogRead(potPin);
   int degrees = map(potValue, 0, 4095, 0, 360);
   float potFloat = degrees / 360.0;
-  // Serial.println(potValue);
-  Serial.print("pot: ");
-  Serial.print(potValue);
-  Serial.print(", degress: ");
-  Serial.print(degrees);
-  Serial.print(", ");
-  Serial.print(potFloat * 100);
-  Serial.println("%");
+  if (countPrint % PRINT_EVERY == 0) {
+    // Serial.println(potValue);
+    Serial.print("pot: ");
+    Serial.print(potValue);
+    Serial.print(", degress: ");
+    Serial.print(degrees);
+    Serial.print(", ");
+    Serial.print(potFloat * 100);
+    Serial.println("%");
+  }
   setColor(potFloat);
   return potValue;
 }
 
 void checkButton() {
   int buttonState = digitalRead(buttonPin);
-  Serial.print("Button: ");     // check if the pushbutton is pressed.
-  Serial.println(buttonState);  // check if the pushbutton is pressed.
+  if (countPrint % PRINT_EVERY == 0) {
+    Serial.print("Button: ");     // check if the pushbutton is pressed.
+    Serial.println(buttonState);  // check if the pushbutton is pressed.
+  }
   if (buttonState == HIGH) {
-    // turn LED on
-    digitalWrite(ledPin, HIGH);
+    Serial.print("Button: ");     // check if the pushbutton is pressed.
+    Serial.println(buttonState);  // check if the pushbutton is pressed.
+                                  // turn LED on
+                                  // digitalWrite(ledPin, HIGH);
+    digitalWrite(buzzer, HIGH);
   } else {
     // turn LED off
-    digitalWrite(ledPin, LOW);
+    digitalWrite(buzzer, LOW);
   }
 }
 
@@ -87,12 +139,14 @@ void setColor(float h) {
   int r = hue2rgb(p, q, h + 1. / 3) * 255;
   int g = hue2rgb(p, q, h) * 255;
   int b = hue2rgb(p, q, h - 1. / 3) * 255;
-  Serial.print("rgb: ");
-  Serial.print(r);
-  Serial.print(", ");
-  Serial.print(g);
-  Serial.print(", ");
-  Serial.println(b);
+  if (countPrint % PRINT_EVERY == 0) {
+    Serial.print("rgb: ");
+    Serial.print(r);
+    Serial.print(", ");
+    Serial.print(g);
+    Serial.print(", ");
+    Serial.println(b);
+  }
   setColor(r, g, b);
 }
 
