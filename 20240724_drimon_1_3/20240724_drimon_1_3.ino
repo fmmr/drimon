dd3c5f31b2a2700da41fafdec082fdae9ef6a52d
Fredrik Rødland <fmr@finn.no>
2024-07-24 11:36:11 +0200

    small incremental change to drimon_1_3 (added sensors and measuring

--- 20240724_drimon_1_3/20240724_drimon_1_3.ino
+++ 20240724_drimon_1_3/20240724_drimon_1_3.ino
@@ -1,7 +1,13 @@
+#include <Adafruit_VL53L0X.h>
 #include <Adafruit_SSD1306.h>
+#include <Adafruit_BME280.h>
+#include <Adafruit_AHTX0.h>
 #include <WiFi.h>
 #include <LCD_I2C.h>
+#include <BH1750.h>
+
 #include <secrets.h>
+#include <sensordata.h>
 // secrets are located in secrets.h
 // #define WIFI_SSID "xxx"
 // #define WIFI_PASSWORD "xxx"
@@ -12,18 +18,28 @@
 #define BLUE_LED_PIN 16
 #define BUZZZER_PIN 32
 #define SENSOR_POWER_PIN 13
-#define POST_SWITCH_PIN 27  // GPIO pin for the secondary button
+#define POST_SWITCH_PIN 27
 
 #define FLASH_WIFI_CONNECT_FAILURE 2
 #define FLASH_DISPLAY_INIT_FAILURE 3
 
+#define SEALEVELPRESSURE_HPA 1013.25
 #define WIFI_MAX_RETRIES 3
+#define NUM_READINGS 2
+#define SLEEP_BETWEEN_READINGS 12
+#define HEIGHT_ABOVE_SEE_LEVEL 31
 
 int dispLine = 0;
 boolean SHOULD_POST = false;
 
+Adafruit_VL53L0X tof = Adafruit_VL53L0X();
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
 LCD_I2C lcd(0x27, 16, 2);
+Adafruit_BME280 bme;
+BH1750 lightMeter;
+Adafruit_AHTX0 aht;
+
+
 // WiFiClient client;
 
 
@@ -56,16 +72,20 @@ void flashLED(int pin, int times, int delayTime = 180) {
   }
 }
 
-void testLeds() {// TODO no need for this
+void testLeds() {  // TODO no need for this
   flashLED(GREEN_LED_PIN, 2);
   flashLED(BLUE_LED_PIN, 2);
   flashLED(RED_LED_PIN, 2);
 }
 
+// TODO - do not post til thingspeak if button is pressed
+// TODO - do not display/power on display if wakeup from timer  (can we turn off lcd backlight)
+
+
 void setup() {
   setupPins();
   Serial.begin(115200);
-  Serial.println("Setup started");
+  Serial.println("Setup...");
 
   initDisplays();
 
@@ -75,14 +95,25 @@ void setup() {
   // TODO we only need wifi if we are to log data
   connectToWiFi();
 
+  initSensors();
+
   // TODO no need for this
-  testLeds();  
+  testLeds();
+  delay(500);
+  // TODO when finished setup? beep(50);
+  Serial.println("Setup: done");
+
+  Serial.println("Measuring...");
+  SensorData data = measure();
+  Serial.println("Measuring: done");
+
+  Serial.println("Displaying data...");
+  displayData(data);
+  Serial.println("Displaying data: done");
 
   delay(5000);
   digitalWrite(SENSOR_POWER_PIN, LOW);
 
-  // TODO when finished setup? beep(50);
-  Serial.println("setup done");
 }
 
 void loop() {  // do nothing
