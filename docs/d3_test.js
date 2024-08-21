679324c84048aec514f1a2b605a6c79d7bf6da17
Fredrik Rødland <fmr@finn.no>
2024-08-21 13:08:25 +0200

    cleaned up d3 script

--- docs/d3_test.js
+++ docs/d3_test.js
@@ -167,81 +167,6 @@ async function drawChart() {
 			svg.selectAll(".tick-line").remove();
 		}
 
-		function setAxisReverse(data) {
-			const timeRange = getTimeRange(data);
-			y = d3.scaleTime()
-				.domain(d3.extent(data, d => d.date))
-				.range([reverse_axis_left, height]);
-
-			svg.append("g")
-				.attr("transform", `translate(5,0)`)
-				.attr("class", "y-axis")
-				.call(d3.axisLeft(y).ticks(5).tickFormat(d => customTickFormat(d, timeRange)).tickSizeOuter(0))
-				.call(g => g.selectAll(".domain, .tick").classed("chart-y-axis-label", true))
-				.lower()
-				.append("text")
-				.attr("transform", "rotate(-90)")
-				.attr("y", -50)
-				.attr("x", -height / 2)
-				.attr("class", "chart-xaxis")
-				.text(decodeURIComponent(''));
-			if (timeRange <= 10 * 60 * 1000) {
-				d3.selectAll(".chart-y-axis-label").style("font-size", "9px");
-			}
-			const xminValue = Math.min(d3.min(data, d => d.value), 0);
-			const xmaxValue = d3.max(data, d => d.value);
-
-			x = d3.scaleLinear()
-				.domain([xminValue, xmaxValue])
-				.range([0, width]);
-
-			let xTicks = x.ticks(5, "s");
-			xTicks.forEach(function(tickValue) {
-				svg.append("line")
-					.attr("class", "tick-line")
-					.attr("x1", x(tickValue))
-					.attr("y1", title_height - 2)
-					.attr("x2", x(tickValue))
-					.attr("y2", height + 1)
-					.attr("stroke", "#e0e0e0")
-					.attr("stroke-width", 1)
-					.attr("transform", `translate(5,0)`)
-					.lower();
-			});
-
-			function customValueTickFormatX(d) {
-				// When the tick label's number is greater than 1e24, 
-				// only the highest two significant digits are retained, 
-				// followed by an "E" at the end. In other cases, the default tick format of d3.js is used.
-				// for example:
-				// 3e+125 -> 3.0E
-				if (d >= tickValueThreshold) {
-					const dStr = d.toExponential();
-					const [base, exponent] = dStr.split('e');
-					const significantDigits = base.replace(/[^0-9]/g, '').slice(0, 3).padEnd(3, '0');
-					const roundedValue = Math.round(parseInt(significantDigits) / 10);
-					const result = roundedValue.toString().padEnd(2, '0');
-
-					return `${result[0]}.${result[1]}E`;
-				} else {
-					return x.tickFormat()(d);
-				}
-			}
-
-			svg.append("g")
-				.attr("transform", `translate(5,${height})`)
-				.attr("class", "x-axis")
-				.call(d3.axisBottom(x).ticks(5, "s").tickFormat(d => customValueTickFormatX(d)).tickSizeOuter(0))
-				.call(g => g.selectAll(".domain, .tick line").remove())
-				.call(g => g.selectAll(".domain, .tick").classed("chart-x-axis-label", true))
-				.lower()
-				.append("text")
-				.attr("y", 35)
-				.attr("x", width / 2)
-				.attr("class", "chart-yaxis")
-				.text(response.channel.field1);
-		}
-
 		// add interactive tooltip
 		const tooltip = d3.select("#chart-container")
 			.append("div")
@@ -306,146 +231,41 @@ async function drawChart() {
 			.attr("width", width)
 			.attr("height", height * 3);
 
-		if (false) {
-			setAxis(data);
+		setAxis(data);
+		svg.append("path")
+			.datum(data)
+			.attr("class", "chart-line")
+			.attr("id", "visualization_chart")
+			.style("stroke", '#d62020')
+			.attr("d", d3.line()
+				.x(d => x(d.date))
+				.y(d => y(d.value))
+			).attr("clip-path", "url(#clip)");
+		svg.append('g')
+			.selectAll("dot")
+			.data(data)
+			.enter()
+			.append("circle")
+			.attr("cx", function(d) {
+				return x(d.date);
+			})
+			.attr("cy", function(d) {
+				return y(d.value);
+			})
+			.attr("r", 3.5)
+			.attr("class", "chart-circle")
+			.attr("fill", '#d62020')
+			.on("mouseover", mouseover("line"))
+			.on("mouseleave", mouseleave)
+			.attr("clip-path", "url(#clip)");
 
-			let minInterval = d3.min(data.slice(1).map((d, i) => x(d.date) - x(data[i].date)));
-			let columnWidth = Math.min(maxBarColumnWidth, Math.max(minInterval * columnWidthRatio, minBarColumnWidth));
-			var columnGroup = svg.append("g")
-				.datum(data)
-				.attr("id", "visualization_chart");
-
-			columnGroup.selectAll("rect")
-				.data(d => d)
-				.enter()
-				.append("rect")
-				.attr("x", d => x(d.date) - 0.5 * columnWidth)
-				.attr("y", d => y(d.value))
-				.attr("width", columnWidth)
-				.attr("height", d => height - y(d.value))
-				.attr("fill", '#d62020')
-				.on("mouseover", mouseover("column"))
-				.on("mouseleave", mouseleave)
-				.attr("clip-path", "url(#clip)");
-
-		} else if (false) {
-			setAxisReverse(data);
-			let minInterval = d3.min(data.slice(1).map((d, i) => y(d.date) - y(data[i].date)));
-			let barWidth = Math.min(maxBarColumnWidth, Math.max(minInterval * barWidthRatio, minBarColumnWidth));
-
-			var barGroup = svg.append("g")
-				.datum(data)
-				.attr("id", "visualization_chart");
-
-			barGroup.selectAll("rect")
-				.data(d => d)
-				.enter()
-				.append("rect")
-				.attr("y", d => y(d.date) - 0.5 * barWidth)
-				.attr("x", 0)
-				.attr("height", barWidth)
-				.attr("width", d => x(d.value))
-				.attr("fill", '#d62020')
-				.attr("transform", `translate(5,0)`)
-				.on("mouseover", mouseover('bar'))
-				.on("mouseleave", mouseleave)
-				.attr("clip-path", "url(#clipReverse)");
-		} else if (false) {
-			setAxis(data);
-			svg.append("path")
-				.datum(data)
-				.attr("class", "chart-line")
-				.attr("id", "visualization_chart")
-				.style("stroke", '#d62020')
-				.attr("d", d3.line()
-					.x(d => x(d.date))
-					.y(d => y(d.value))
-					.curve(d3.curveCardinal.tension(splineTension))
-				).attr("clip-path", "url(#clip)");
-
-			svg.append('g')
-				.selectAll("dot")
-				.data(data)
-				.enter()
-				.append("circle")
-				.attr("cx", function(d) {
-					return x(d.date);
-				})
-				.attr("cy", function(d) {
-					return y(d.value);
-				})
-				.attr("r", 3.5)
-				.attr("class", "chart-circle")
-				.attr("fill", '#d62020')
-				.on("mouseover", mouseover("spline"))
-				.on("mouseleave", mouseleave)
-				.attr("clip-path", "url(#clip)");
-		} else if (false) {
-			setAxis(data);
-			svg.append("path")
-				.datum(data)
-				.attr("class", "chart-line")
-				.attr("id", "visualization_chart")
-				.style("stroke", '#d62020')
-				.attr("d", d3.line()
-					.x(d => x(d.date))
-					.y(d => y(d.value))
-					.curve(d3.curveStepAfter)
-				).attr("clip-path", "url(#clip)");
-
-			svg.append('g')
-				.selectAll("dot")
-				.data(data)
-				.enter()
-				.append("circle")
-				.attr("cx", function(d) {
-					return x(d.date);
-				})
-				.attr("cy", function(d) {
-					return y(d.value);
-				})
-				.attr("r", 3.5)
-				.attr("class", "chart-circle")
-				.attr("fill", '#d62020')
-				.on("mouseover", mouseover("step"))
-				.on("mouseleave", mouseleave)
-				.attr("clip-path", "url(#clip)");
-		} else {
-			setAxis(data);
-			svg.append("path")
-				.datum(data)
-				.attr("class", "chart-line")
-				.attr("id", "visualization_chart")
-				.style("stroke", '#d62020')
-				.attr("d", d3.line()
-					.x(d => x(d.date))
-					.y(d => y(d.value))
-				).attr("clip-path", "url(#clip)");
-			svg.append('g')
-				.selectAll("dot")
-				.data(data)
-				.enter()
-				.append("circle")
-				.attr("cx", function(d) {
-					return x(d.date);
-				})
-				.attr("cy", function(d) {
-					return y(d.value);
-				})
-				.attr("r", 3.5)
-				.attr("class", "chart-circle")
-				.attr("fill", '#d62020')
-				.on("mouseover", mouseover("line"))
-				.on("mouseleave", mouseleave)
-				.attr("clip-path", "url(#clip)");
-		}
 
 		let calculatePosX = function(x) {
 			return x > 200 ? x - 200 : x + 20;
 		}
 
 		let calculatePosY = function(y) {
-			return y > height - 40 ? height - 40 : y;
+			return y > height - 30 ? height - 30 : y;
 		}
 
 		// copyright
@@ -453,213 +273,80 @@ async function drawChart() {
 			.attr("x", width)
 			.attr("y", height + margin.bottom * 5 / 6)
 			.attr("class", "chart-copyright")
-			.text("ThingSpeak.com")
+			.text("drimon")
 			.on("click", () => {
-				window.top.location.href = "https://thingspeak.com/";
+				window.top.location.href = "https://drimon.rodland.no/";
 			});
 
 		$('#loader-gif').remove();
 
 		let url = 'https://api.thingspeak.com/channels/2568299/feed/last.json?&amp;offset=0&amp;location=true&amp;timezone=Europe/Paris&amp;start=2024-08-18 00:00:00&amp;results=8000&amp;round=2';
-		if ("".length > 0) {
-			url = 'https://api.thingspeak.com/channels/2568299/feed/last_average.json?&amp;offset=0&amp;location=true&amp;average=&amp;timezone=Europe/Paris&amp;start=2024-08-18 00:00:00&amp;results=8000&amp;round=2';
-		} else if ("".length > 0) {
-			url = 'https://api.thingspeak.com/channels/2568299/feed/last_median.json?&amp;offset=0&amp;location=true&amp;median=&amp;timezone=Europe/Paris&amp;start=2024-08-18 00:00:00&amp;results=8000&amp;round=2';
-		} else if ("".length > 0) {
-			url = 'https://api.thingspeak.com/channels/2568299/feed/last_sum.json?&amp;offset=0&amp;location=true&amp;sum=&amp;timezone=Europe/Paris&amp;start=2024-08-18 00:00:00&amp;results=8000&amp;round=2';
-		}
 
-		if ('true' === 'true' && (''.length < 1)) {
-			setInterval(() => {
-				d3.json(url).then(
-					function(updatedata) {
-						if (updatedata && updatedata.field1) {
-							if (data.length > 0) {
-								last_date = data[data.length - 1].date;
-							}
-							const newpoint = {
-								date: Date.parse(updatedata.created_at),
-								value: parseFloat(updatedata.field1)
-							};
-							let shift = false; //default for shift
-							let results = 8000;
-							if (results && data.length + 1 >= results) {
-								shift = true;
-							}
-							if (!isNaN(newpoint.value) && (newpoint.date != last_date)) {
-								if (shift) {
-									data.shift();
-								}
-								data.push(newpoint);
-							} else {
-								data[data.length - 1] = newpoint;
-							}
-
-							if (false) {
-								removeAxis();
-								setAxis(data);
-								let minInterval = d3.min(data.slice(1).map((d, i) => x(d.date) - x(data[i].date)));
-								let columnWidth = Math.min(maxBarColumnWidth, Math.max(minInterval * columnWidthRatio, minBarColumnWidth));
-
-								columnGroup.selectAll("rect")
-									.data(data)
-									.join(
-										enter => enter.append("rect")
-										.attr("x", d => x(d.date) - 0.5 * columnWidth)
-										.attr("y", d => y(d.value))
-										.attr("width", columnWidth)
-										.attr("height", d => height - y(d.value))
-										.attr("fill", '#d62020')
-										.on("mouseover", mouseover("column"))
-										.on("mouseleave", mouseleave)
-										.attr("clip-path", "url(#clip)"),
-										update => update
-										.attr("x", d => x(d.date) - 0.5 * columnWidth)
-										.attr("y", d => y(d.value))
-										.attr("width", columnWidth)
-										.attr("height", d => height - y(d.value))
-										.attr("fill", '#d62020')
-										.on("mouseover", mouseover("column"))
-										.on("mouseleave", mouseleave)
-										.attr("clip-path", "url(#clip)"),
-										exit => exit.remove()
-									);
-
-							} else if (false) {
-								removeAxis();
-								setAxisReverse(data);
-								let minInterval = d3.min(data.slice(1).map((d, i) => y(d.date) - y(data[i].date)));
-								let barWidth = Math.min(maxBarColumnWidth, Math.max(minInterval * barWidthRatio, minBarColumnWidth));
-
-								barGroup.selectAll("rect")
-									.data(data)
-									.join(
-										enter => enter.append("rect")
-										.attr("y", d => y(d.date) - 0.5 * barWidth)
-										.attr("x", 0)
-										.attr("height", barWidth)
-										.attr("width", d => x(d.value))
-										.attr("fill", '#d62020')
-										.attr("transform", `translate(5,0)`)
-										.on("mouseover", mouseover("bar"))
-										.on("mouseleave", mouseleave)
-										.attr("clip-path", "url(#clipReverse)"),
-										update => update
-										.attr("y", d => y(d.date) - 0.5 * barWidth)
-										.attr("x", 0)
-										.attr("height", barWidth)
-										.attr("width", d => x(d.value))
-										.attr("fill", '#d62020')
-										.attr("transform", `translate(5,0)`)
-										.on("mouseover", mouseover("bar"))
-										.on("mouseleave", mouseleave)
-										.attr("clip-path", "url(#clipReverse)"),
-										exit => exit.remove()
-									);
-
-							} else if (false) {
-								removeAxis();
-								setAxis(data);
-								svg.select("#visualization_chart")
-									.datum(data)
-									.style("stroke", '#d62020')
-									.attr("d", d3.line()
-										.x(d => x(d.date))
-										.y(d => y(d.value))
-										.curve(d3.curveCardinal.tension(splineTension))
-									).attr("clip-path", "url(#clip)");
-								svg.selectAll("circle")
-									.data(data)
-									.join(
-										enter => enter.append("circle")
-										.attr("cx", d => x(d.date))
-										.attr("cy", d => y(d.value))
-										.attr("r", 3.5)
-										.attr("class", "chart-circle")
-										.attr("fill", '#d62020')
-										.on("mouseover", mouseover("spline"))
-										.on("mouseleave", mouseleave)
-										.attr("clip-path", "url(#clip)"),
-										update => update
-										.attr("cx", d => x(d.date))
-										.attr("cy", d => y(d.value))
-										.on("mouseover", mouseover("spline"))
-										.on("mouseleave", mouseleave)
-										.attr("clip-path", "url(#clip)"),
-										exit => exit.remove()
-									);
-
-							} else if (false) {
-								removeAxis();
-								setAxis(data);
-								svg.select("#visualization_chart")
-									.datum(data)
-									.style("stroke", '#d62020')
-									.attr("d", d3.line()
-										.x(d => x(d.date))
-										.y(d => y(d.value))
-										.curve(d3.curveStepAfter)
-									).attr("clip-path", "url(#clip)");
-								svg.selectAll("circle")
-									.data(data)
-									.join(
-										enter => enter.append("circle")
-										.attr("cx", d => x(d.date))
-										.attr("cy", d => y(d.value))
-										.attr("r", 3.5)
-										.attr("class", "chart-circle")
-										.attr("fill", '#d62020')
-										.on("mouseover", mouseover("spline"))
-										.on("mouseleave", mouseleave)
-										.attr("clip-path", "url(#clip)"),
-										update => update
-										.attr("cx", d => x(d.date))
-										.attr("cy", d => y(d.value))
-										.on("mouseover", mouseover("spline"))
-										.on("mouseleave", mouseleave)
-										.attr("clip-path", "url(#clip)"),
-										exit => exit.remove()
-									);
-							} else {
-								removeAxis();
-								setAxis(data);
-								svg.select("#visualization_chart")
-									.datum(data)
-									.style("stroke", '#d62020')
-									.attr("d", d3.line()
-										.x(d => x(d.date))
-										.y(d => y(d.value))
-									).attr("clip-path", "url(#clip)");
-								svg.selectAll("circle")
-									.data(data)
-									.join(
-										enter => enter.append("circle")
-										.attr("cx", d => x(d.date))
-										.attr("cy", d => y(d.value))
-										.attr("r", 3.5)
-										.attr("fill", '#d62020')
-										.attr("class", "chart-circle")
-										.on("mouseover", mouseover("line"))
-										.on("mouseleave", mouseleave)
-										.attr("clip-path", "url(#clip)"),
-										update => update
-										.attr("cx", d => x(d.date))
-										.attr("cy", d => y(d.value))
-										.attr("fill", '#d62020')
-										.on("mouseover", mouseover("line"))
-										.on("mouseleave", mouseleave)
-										.attr("clip-path", "url(#clip)"),
-										exit => exit.remove()
-									);
-							}
+		setInterval(() => {
+			d3.json(url).then(
+				function(updatedata) {
+					if (updatedata && updatedata.field1) {
+						if (data.length > 0) {
+							last_date = data[data.length - 1].date;
 						}
+						const newpoint = {
+							date: Date.parse(updatedata.created_at),
+							value: parseFloat(updatedata.field1)
+						};
+						let shift = false; //default for shift
+						let results = 8000;
+						if (results && data.length + 1 >= results) {
+							shift = true;
+						}
+						if (!isNaN(newpoint.value) && (newpoint.date != last_date)) {
+							if (shift) {
+								data.shift();
+							}
+							data.push(newpoint);
+						} else {
+							data[data.length - 1] = newpoint;
+						}
+
+						removeAxis();
+						setAxis(data);
+						svg.select("#visualization_chart")
+							.datum(data)
+							.style("stroke", '#d62020')
+							.attr("d", d3.line()
+								.x(d => x(d.date))
+								.y(d => y(d.value))
+							).attr("clip-path", "url(#clip)");
+
+						svg.selectAll("circle")
+							.data(data)
+							.join(
+								enter => enter.append("circle")
+								.attr("cx", d => x(d.date))
+								.attr("cy", d => y(d.value))
+								.attr("r", 3.5)
+								.attr("fill", '#d62020')
+								.attr("class", "chart-circle")
+								.on("mouseover", mouseover("line"))
+								.on("mouseleave", mouseleave)
+								.attr("clip-path", "url(#clip)"),
+								update => update
+								.attr("cx", d => x(d.date))
+								.attr("cy", d => y(d.value))
+								.attr("fill", '#d62020')
+								.on("mouseover", mouseover("line"))
+								.on("mouseleave", mouseleave)
+								.attr("clip-path", "url(#clip)"),
+								exit => exit.remove()
+							);
 					}
-				);
-			}, 15000);
-		}
+
+				}
+			);
+		}, 15000);
+
 
 	} catch (error) {
-console.log("hey!")
-console.log(error)
+		console.log("got an error!")
+		console.log(error)
 	}
 }
